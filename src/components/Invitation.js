1ea72bc65c29cc938798fa129d53a564e5e5bb49
ArielFeingold <feingold.ariel@gmail.com>
2018-07-31 10:34:41 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,15 @@
-// Code Invitation Component Here
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export default class Invitation extends React.Component {
+  render() {
+    return(
+      <div className="invetation">
+        <h1>You've been invited!</h1>
+        <div className="invetationDetails">{this.props.children}</div>
+      </div>
+    )
+  }
+
+
+}
